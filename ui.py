dc1c219c23d64934f914fdadc8302d3f3ca69693
arpan8925 <sbxp1966@gmail.com>
2024-06-19 19:55:35 +0530

    Background

--- ui.py
+++ ui.py
@@ -6,7 +6,10 @@ class AppUi:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
-
+        self.window.config(padx=50, pady=50, background=THEME_COLOR)
+        self.canvas = Canvas()
+        self.canvas.config(width=300, height=550)
+        self.canvas.grid(row=0, column=0)
 
 
 
